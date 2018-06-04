985b58788b2268fa0418a9e39ba23e36d29b9726
alexlueng <alexlueng@icloud.com>
2018-06-04 08:43:13 +0800

    a tired weekend

--- author/views.py
+++ author/views.py
@@ -1,5 +1,43 @@
 from flask_blog import app
+from flask import render_template, redirect, url_for, session, request
+from author.form import RegisterForm, LoginForm
+from author.models import Author
+from author.decorators import login_required
 
-@app.route('/login')
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-	return "Hello authors!"
+	form = LoginForm()
+	error = None
+
+	if request.method == 'GET' and request.args.get('next'):
+		session['next'] = request.args.get('next', None)
+	if form.validate_on_submit():
+		author = Author.query.filter_by(username=form.username.data, password=form.password.data).limit(1)
+		print(author)
+		if author.count():
+			session['username'] = form.username.data
+			if 'next' in session:
+				next = session.get('next')
+				session.pop('next')
+				return redirect(next)
+			else:
+				return redirect(url_for('login_success'))
+		else:
+			error = "Incorrect username and password"
+	return render_template('author/login.html', form=form, error=error)
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		return redirect(url_for('success'))
+	return render_template('author/register.html', form=form)
+
+@app.route('/success')
+def success():
+	return "Author registered success"
+
+@login_required
+@app.route('/login_success')
+def login_success():
+	return "Author login success"
--- settings.py
+++ settings.py
@@ -8,7 +8,8 @@ DB_PASSWORD = 'turkey414'
 BLOG_DATABASE_NAME = 'blog'
 
 DB_HOST = os.getenv('IP', '127.0.0.1')
-DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+#DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+DB_URI = 'mysql+pymysql://root:turkey414@localhost/blog'
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
